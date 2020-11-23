208bc89ceebb3235085acfe648257c08be817985
2dam3 <iker_m123@hotmail.es>
2020-11-23 12:56:34 +0100

    first commit

---
+++ lib/Api/ApiController.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+const baseUrl = "https://randomuser.me/api/";
+
+class API {
+  static Future getUsers() {
+    var url = baseUrl + "/?results=20&nat=es";
+    return http.get(url);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+import 'package:listaApiFlutter/Api/ApiController.dart';
+import 'package:listaApiFlutter/Model/User.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  build(context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Http App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyListScreen(),
+    );
+  }
+}
+
+class MyListScreen extends StatefulWidget {
+  @override
+  createState() => _MyListScreenState();
+}
+
+class _MyListScreenState extends State {
+  var users = new List<User>();
+
+  _getUsers() {
+    API.getUsers().then((response) {
+      setState(() {
+        Iterable list = json.decode(response.body);
+        users = list.map((model) => User.fromJson(model)).toList();
+      });
+    });
+  }
+
+  initState() {
+    super.initState();
+    _getUsers();
+  }
+
+  dispose() {
+    super.dispose();
+  }
+
+  @override
+  build(context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("User List"),
+        ),
+        body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                //leading: FlutterLogo(size: 72.0),
+                title: Text(users[index].name),
+                subtitle:
+                    Text(users[index].email + "" + users[index].id.toString()),
+
+                isThreeLine: true,
+              ),
+            );
+          },
+        ));
+  }
+}
